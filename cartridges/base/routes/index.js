5d5d0942180d09ea70ee4073fe2cd1e951e059b4
Sergey Lozhechnikov <s.lozhechnikov@astoundcommerce.com>
2020-10-13 14:53:53 +0300

    db test

--- cartridges/base/routes/index.js
+++ cartridges/base/routes/index.js
@@ -1,5 +1,19 @@
 $ = require('liteql');
 
 module.exports = {
-	index : ($)=>{return {"hello":"world"}}
+	index : ($)=>{return {"hello":"world"}},
+	testDB : ($)=>{
+		const { Pool, Client } = require('pg')
+		// pools will use environment variables
+		// for connection information
+		const pool = new Pool()
+		return pool.query('SELECT NOW()').then((res)=>{
+			console.log(res);
+			return "success";
+		}).catch(err=>{
+			console.log("WAAAAAT");
+			console.log(err);
+			return "fail"
+		});
+	}
 }
