56a509958318f363d7c5b4ea0d08ca986cf8db26
Sergey <s.lozhechnikov@gmail.com>
2020-10-18 20:21:29 +0300

    set schedule in progress

---
+++ cartridges/base/resources/calendar.js
@@ -0,0 +1,18 @@
+
+
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = APP_ROOT+'/buffer/token.json';
+
+module.exports = {
+	index(query){
+
+	}
+}
--- cartridges/base/routes/calendar.js
+++ cartridges/base/routes/calendar.js
@@ -1,7 +1,48 @@
 module.exports = {
 	setSchedule : (scope)=>{
 		return require(APP_ROOT+"/modules/app")("model").get("Therapist", scope.req.query.id, scope.session).then(therapist=>{
-			return scope.session.getVar("liteql").call({"!base_template":["setSchedule", therapist.obj]});
+			var data = {
+					thereapist : therapist.obj
+				},
+				now = new Date(),
+				months = [],
+				monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+			for (let i = 1; i <= 3; i++){
+				let currentMonth = now.getMonth(),
+					days = [];
+				for(let j = 1; j<=31; j++){		
+					let hours = [];
+					for(let k = 0; k < 24; k++){
+						hours.push({
+							hour : k,
+							fromTo : k + " - "+(k+1),
+							work : true
+						});
+					}			
+					days.push({
+						date : now.getDate(),
+						day : now.getDay(),
+						hours : hours
+					});
+					now.setDate(now.getDate()+1);
+					if(now.getMonth() != currentMonth) break;
+
+				}
+				now.setMonth(now.getMonth()-1);	
+				months.push({
+					MY : now.getMonth()+"|"+now.getFullYear(),
+					M : monthNames[now.getMonth()],
+					days : days
+				});
+				now.setMonth(now.getMonth()+1);
+
+			}
+			return scope.session.getVar("liteql").call({
+				"!base_template" : ["setSchedule", {
+					therapist : therapist.obj,
+					months : months
+				}]
+			});
 		});
 	}
 }
