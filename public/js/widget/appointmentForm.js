b794cb5052ba414d170933978e8ab5dad15e2fcd
Sergey <s.lozhechnikov@gmail.com>
2021-01-20 16:33:46 +0200

    fixed date problem

--- public/js/widget/appointmentForm.js
+++ public/js/widget/appointmentForm.js
@@ -100,7 +100,7 @@
                             therapist : document.getElementById("appointment_form_container").dataset.therapist,
                             name : document.getElementById("appointment_name").value,
                             phone : document.getElementById("appointment_phone").value,
-                            date : picker.selectedDate,
+                            date : picker.dateSelected,
                             time : document.getElementById("appiontment_time").value,
                             howToCall : document.getElementById("how_to_call").value
                         };
