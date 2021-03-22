175d2d77cc332732a72a1551816e7cfdc87c6261
Sergey <s.lozhechnikov@gmail.com>
2021-03-22 11:05:50 +0200

    fix telegram messanging

--- cartridges/base/resources/appointment.js
+++ cartridges/base/resources/appointment.js
@@ -22,7 +22,6 @@ module.exports = {
                 error: "invalide_date"
             }
         }
-        console.log(date);
         return require(APP_ROOT+"/modules/app")("model").get("Appointment").then(Appointment=>{
             return Appointment.submit({
                 therapistID : therapistID,
@@ -40,15 +39,11 @@ module.exports = {
                     return res;
                 }
                 return currentInstance.scope.$.call([
-                    {"storage_therapist>therapist":[therapistID]},
-                    {"base_msg>msg" : ["mail", ["\\w*"]]},
-                    {"base_template>body" : ""}
+                    {"storage_therapist>therapist":[therapistID]}
                 ]).then(mailData=>{
                     return require(APP_ROOT+"/modules/app")("utils", "email").send({
-                        to : mailData.therapist.email,
-                        body : mailData.body,
-                        subject : mailData.msg.appointmentNewSubject
-                    }).then(info=>{
+                        to : mailData.therapist.email
+                    }).then(info=>{                    
                         LOGGER.debug("Email is sent. Details: "+JSON.stringify(info));
                             return mailData.therapist.tg_id ? require(APP_ROOT+"/modules/app")("utils", "msg").send({
                                 chat_id : mailData.therapist.tg_id, 
