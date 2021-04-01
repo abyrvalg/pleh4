78b0fa0ae9d4a2246130f33e9f1ee9f8bdb05c3d
Eugen Torrent <39799304+PatiHox@users.noreply.github.com>
2021-04-01 17:15:40 +0300

    Added log-read ability.
    Still got things to do on this.
    -I returned and modified indexFiles() function. (bc reasons)
    -Added read line-by-line functionality
    -I wanted to make file read output synced, but that was hard, so i settled on a promise
    -For consistency i now have to make return of logs list a promise щ（ﾟДﾟщ）

--- core/logReader.js
+++ core/logReader.js
@@ -1,20 +1,22 @@
 const PATH = require("path");
 const LOGPATH = PATH.resolve("logs");
-const CONFIG = require("./../config");
+const readline = require("readline");
 const chokidar = require("chokidar");
 const LOGGER = require(APP_ROOT + "/modules/app")("logger");
 const FS = require("fs");
 
-var streams = {};
 var cachedFileIndexes = [];
 
+// Index files on invoke
+indexFiles();
 // Used to watch over an addition or removal of the log files
-// Chokidar fires "add" events on ALL files when watching is getting started
+// Chokidar fires "add" events on all files when watching is getting started
 // -----------------------------
 chokidar
   .watch(LOGPATH)
   .on("add", (path, eventName) => {
     let fileName = path.match(/(?:.+\\)(.+)$/)[1];
+    if (!fileName.match(/^(\w+)-(\d+)_(\d+)_(\d+)(?:\.\w{3})$/)) return;
     // ----- DEBUG -----
     LOGGER.debug(
       "Chokidar's add event fired!" + "\n#####\n" + "fileName: " + fileName
@@ -61,58 +63,134 @@ chokidar
 // -----------------------------
 
 function readLogs(type, index) {
-  return index
-    ? (type, index) => {
-        //TODO: add returns for file and fileList
+  // If index is null -> returns list of logs
+  // Else if index is set -> returns content of log file
+  if (index !== null) {
+    // Find entry by index
+    let fileIndex = cachedFileIndexes.find((value, i, arr) => {
+      return index == value.index;
+    });
+    // Check if file entry is invalid (file wasn't indexed)
+    if (!fileIndex) {
+      LOGGER.warn(
+        "Attempt to access not indexed file with index '" + index + "'"
+      );
+      return null;
+    }
+    // Creation of base return object
+    var ret = {
+      filename: fileIndex.path,
+      lines: [],
+    };
+    // ----- DEBUG -----
+    LOGGER.debug(
+      "Trying to start reading file: " + PATH.resolve(LOGPATH, fileIndex.path)
+    );
+    // ----- DEBUG -----
+    
+
+    return new Promise((resolve, reject)=>{
+		const fileStream = FS.createReadStream(
+			PATH.resolve(LOGPATH, fileIndex.path)
+		  );
+		  fileStream.on("error", (err)=>{
+			reject(err);
+		  });
+		  const rl = readline.createInterface({
+			input: fileStream,
+			crlfDelay: Infinity,
+		  });
+		  let cache;
+		  rl.on('line', (line) => {
+			if (cache) {
+			  let match = line.match(
+				/^(?:(?:\w{3}\s){2}(?:\d+\s){2}((?:\d+:?){3})\s(\w+\+\w+))/
+			  );
+			  if (match) {
+				ret.lines.push(cache);
+				cache = line;
+			  } else {
+				cache += "\n" + line;
+			  }
+			} else cache = line;
+		  });
+		  rl.on('close', (input) => {
+			  resolve(ret);
+		  });
+	});
+  } else {
+	// TODO: This thing is too easy... WHY DON'T I CONVERT IT INTO PROMISE????
+	// (but i actually think that converting it to promise will make the code more consistant)
+    var ret = [];
+    cachedFileIndexes.forEach((fileIndex, i, arr) => {
+      let fileName = fileIndex.path;
+      let index = fileIndex.index;
+      fileName = fileName.match(/^(\w+)-(\d+).(\d+).(\d+)(?:\.\w{3})$/);
+      if (type == fileName[1]) {
+        let day = fileName[2];
+        let month = fileName[3];
+        let year = fileName[4];
+        ret.push({
+          fileName: fileName[0],
+          index: index,
+          date: day + "-" + month + "-" + year,
+        });
       }
-    : null;
+    });
+    return ret;
+  }
 }
 
 function indexFiles() {
   !FS.existsSync(LOGPATH) && FS.mkdirSync(LOGPATH);
   let files = FS.readdirSync(LOGPATH);
   files.forEach((value, index, arr) => {
-    cachedFileIndexes.push({
-      path: value,
-      index: index,
-    });
+    if (value.match(/^(\w+)-(\d+)_(\d+)_(\d+)(?:\.\w{3})$/))
+      cachedFileIndexes.push({
+        path: value,
+        index: index,
+      });
+    else LOGGER.warn("Found non-log file (or filename is incorrect): " + value);
   });
   // ----- DEBUG -----
-  LOGGER.debug(
-    "Indexed " + cachedFileIndexes.length + " log files on startup!"
-  );
+  LOGGER.debug("Indexed " + cachedFileIndexes.length + " log files on invoke!");
   // ----- DEBUG -----
 }
 
 module.exports = {
-  // Returns array of objects where each object represent a row from logs
+  // Returns object with name of current file and array
+  // of objects where each object represent a row
   // Structure of return array:
-  //  [
+  //  {
+  //  filename,
+  //  lines:
+  //  	[
   //      {
   //          time,
   //          text
   //      },
   //      ...
-  //  ]
+  //    ]
+  //  }
   // ------------
   error(index) {
-    readLogs("error", index);
+    return readLogs("error", index);
   },
   warn(index) {
-    readLogs("warn", index);
+    return readLogs("warn", index);
   },
   debug(index) {
-    readLogs("debug", index);
+    return readLogs("debug", index);
   },
   fatal(index) {
-    readLogs("fatal", index);
+    return readLogs("fatal", index);
   },
   // ------------
-
   // Returns array of object where each object represents a log file
   // Structure of return array:
   //  [
   //      {
+  //		  filename,
   //          index(number),
   //          date
   //      },
@@ -120,16 +198,16 @@ module.exports = {
   //  ]
   // ------------
   errorAll() {
-    readLogs("error", null);
+    return readLogs("error", null);
   },
   warnAll() {
-    readLogs("warn", null);
+    return readLogs("warn", null);
   },
   debugAll() {
-    readLogs("debug", null);
+    return readLogs("debug", null);
   },
   fatalAll() {
-    readLogs("fatal", null);
+    return readLogs("fatal", null);
   },
   // ------------
 };
--- test.js
+++ test.js
@@ -9,4 +9,15 @@ LOGGER.debug("Trying to require logReader...");
 var logReader = require(APP_ROOT+"/modules/app")("logReader");
 LOGGER.debug("logReader require successful");
 
+LOGGER.debug("Trying to read logs 'debug'");
+// Gets logs array object
+let logs = logReader.debugAll();
+LOGGER.debug("Success! Got logs object:" + JSON.stringify(logs));
 
+LOGGER.debug("Trying to read file contents of file with index: " + logs[0].index + " and name: " + logs[0].fileName);
+let content = logReader.debug(logs[0].index);
+content.then((content) => {
+    LOGGER.debug("Success! Got next content: " + JSON.stringify(content));
+}).catch((err)=>{
+    LOGGER.error(err);
+});
