15bc97235bcbd2676405f8eae4749d22f59a3fde
Sergey <s.lozhechnikov@gmail.com>
2021-10-05 15:01:26 +0300

    redirect to https

--- cartridges/base/hooks/onRequest.js
+++ cartridges/base/hooks/onRequest.js
@@ -1,8 +1,5 @@
 module.exports = (req, res, next)=>{
-    next();
-    return;
-    if(req.protocol == "http" && process.env.instance !== "localhost") {
-       // console.log(req.protocol);
+    if(req.protocol == "http" && process.env.instance === "prod") {
         res.redirect("https://"+ req.get('host') + req.originalUrl);
     } else {
         next();
