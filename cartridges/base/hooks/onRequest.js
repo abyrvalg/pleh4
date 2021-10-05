786924b3da22af27b72bc456d2b0d22cdd20382a
Sergey <s.lozhechnikov@gmail.com>
2021-10-05 14:49:34 +0300

    logging

--- cartridges/base/hooks/onRequest.js
+++ cartridges/base/hooks/onRequest.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next)=>{
     if(req.protocol == "http" && process.env.instance !== "localhost") {
+        console.log(req.protocol);
         res.redirect("https://"+ req.get('host') + req.originalUrl);
     } else {
         next();
