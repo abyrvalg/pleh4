b35ed054bac60e62b6c97e31d27fe3c243dc9d48
Sergey <s.lozhechnikov@gmail.com>
2021-10-05 14:52:39 +0300

    no redirect to https

--- cartridges/base/hooks/onRequest.js
+++ cartridges/base/hooks/onRequest.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next)=>{
+    next();
+    return;
     if(req.protocol == "http" && process.env.instance !== "localhost") {
-        console.log(req.protocol);
+       // console.log(req.protocol);
         res.redirect("https://"+ req.get('host') + req.originalUrl);
     } else {
         next();
